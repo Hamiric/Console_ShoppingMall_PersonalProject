4581b12b47d08dcaca156bcec148444166937b44
Hamiric <do9870@naver.com>
2024-10-30 20:24:24 +0900

    Fix : "Mac 환경에서 한글입력 되는것 확인 및 수정"
    Window 환경에서 한글입력에 인코딩쪽 문제가 있어, 맥에서 한글입력되는것 확인
    추가로 소스코드 일부 수정

--- lib/shoppingmall.dart
+++ lib/shoppingmall.dart
@@ -7,11 +7,11 @@ class Shoppingmall {
 
   Shoppingmall() {
     items = [
-      Product('shirt', 45000),  // 셔츠
-      Product('dress', 30000),  // 원피스
-      Product('sleeves', 35000),  // 반팔티
-      Product('shorts', 38000),  // 반바지
-      Product('socks', 5000),  // 양말
+      Product('셔츠', 45000),  // 셔츠, shirt
+      Product('원피스', 30000),  // 원피스, dress
+      Product('반팔티', 35000),  // 반팔티, sleeves
+      Product('반바지', 38000),  // 반바지, shorts
+      Product('양말', 5000),  // 양말, socks
       // Product('sky', 2000)
     ];
   }
